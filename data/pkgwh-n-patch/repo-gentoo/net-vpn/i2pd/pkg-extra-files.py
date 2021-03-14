91e2589fefc3a6c33438f987845e3b9a97ce482d
fpemud <fpemud@sina.com>
2021-03-14 13:28:55 +0800

    fix

--- data/pkgwh-n-patch/repo-gentoo/net-vpn/i2pd/pkg-extra-files.py
+++ data/pkgwh-n-patch/repo-gentoo/net-vpn/i2pd/pkg-extra-files.py
@@ -3,6 +3,9 @@
 
 import glob
 
+# package installs many files into /var/lib/i2pd
+# so we can't have a remove-var.py
+
 for fn in glob.glob("*.ebuild"):
     with open(fn, "a") as f:
         f.write("""
