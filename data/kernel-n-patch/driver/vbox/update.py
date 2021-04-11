5215ef1dea03970221511e60a291dd96be925961
fpemud <fpemud@sina.com>
2021-04-12 04:51:29 +0800

    fix

--- data/kernel-n-patch/driver/vbox/update.py
+++ data/kernel-n-patch/driver/vbox/update.py
@@ -1,62 +1,64 @@
 #!/usr/bin/python3
 
-import os
-import sys
-import glob
-import time
-import shutil
-import subprocess
-import lxml.html
-import urllib.request
-import robust_layer.wget
+# this code is commented because it's too risky, see FIXMEs below
 
-url = "https://www.virtualbox.org/wiki/Linux_Downloads"
-origfile = "./original-file.txt"
+# import os
+# import sys
+# import glob
+# import time
+# import shutil
+# import subprocess
+# import lxml.html
+# import urllib.request
+# import robust_layer.wget
 
-# get download url
-downloadUrl = None
-origFile = None
-while True:
-    try:
-        resp = urllib.request.urlopen(url, timeout=robust_layer.TIMEOUT)
-        root = lxml.html.parse(resp)
-        for link in root.xpath(".//a"):
-            if link.get("href").endswith("_amd64.run"):
-                downloadUrl = link.get("href")
-                origFile = os.path.basename(downloadUrl)
-                break
-        break
-    except Exception as e:
-        print("Failed to acces %s, %s" % (url, e))
-        time.sleep(1.0)
-if downloadUrl is None:
-    raise Exception("failed to download VirtualBox driver")
+# url = "https://www.virtualbox.org/wiki/Linux_Downloads"
+# origfile = "./original-file.txt"
 
-# already downloaded?
-if os.path.exists(origfile):
-    with open(origfile, "r") as f:
-        if f.read() == origFile:
-            print("File already downloaded.")
-            sys.exit(0)
+# # get download url
+# downloadUrl = None
+# origFile = None
+# while True:
+#     try:
+#         resp = urllib.request.urlopen(url, timeout=robust_layer.TIMEOUT)
+#         root = lxml.html.parse(resp)
+#         for link in root.xpath(".//a"):
+#             if link.get("href").endswith("_amd64.run"):
+#                 downloadUrl = link.get("href")
+#                 origFile = os.path.basename(downloadUrl)
+#                 break
+#         break
+#     except Exception as e:
+#         print("Failed to acces %s, %s" % (url, e))
+#         time.sleep(1.0)
+# if downloadUrl is None:
+#     raise Exception("failed to download VirtualBox driver")
 
-# download and extract files
-subprocess.run("/bin/rm -rf ./*", shell=True)       # FIXME: dangerous without sandboxing
-tmpdir = "./__temp__"
-os.mkdir(tmpdir)
+# # already downloaded?
+# if os.path.exists(origfile):
+#     with open(origfile, "r") as f:
+#         if f.read() == origFile:
+#             print("File already downloaded.")
+#             sys.exit(0)
 
-os.chdir(tmpdir)
-try:
-    subprocess.run(["/usr/bin/wget", "-q", "--show-progress", *robust_layer.wget.additional_param(), "-O", "vbox.run", downloadUrl])
-    subprocess.run(["/bin/sh", "vbox.run", "--noexec", "--keep", "--nox11"])        # FIXME: security vulnerbility
-    subprocess.run(["/bin/tar", "-xjf", os.path.join(tmpdir, "install", "VirtualBox.tar.bz2")])
-finally:
-    os.chdir("..")
+# # download and extract files
+# subprocess.run("/bin/rm -rf ./*", shell=True)       # FIXME: dangerous without sandboxing
+# tmpdir = "./__temp__"
+# os.mkdir(tmpdir)
 
-for fn in glob.glob(os.path.join(tmpdir, "src", "vboxhost", "*")):
-    os.rename(fn, ".")
+# os.chdir(tmpdir)
+# try:
+#     subprocess.run(["/usr/bin/wget", "-q", "--show-progress", *robust_layer.wget.additional_param(), "-O", "vbox.run", downloadUrl])
+#     subprocess.run(["/bin/sh", "vbox.run", "--noexec", "--keep", "--nox11"])        # FIXME: security vulnerbility
+#     subprocess.run(["/bin/tar", "-xjf", os.path.join(tmpdir, "install", "VirtualBox.tar.bz2")])
+# finally:
+#     os.chdir("..")
 
-with open(origfile, "w") as f:
-    f.write(origFile)
+# for fn in glob.glob(os.path.join(tmpdir, "src", "vboxhost", "*")):
+#     os.rename(fn, ".")
 
-# keep tmpdir when error occured
-shutil.rmtree(tmpdir)
+# with open(origfile, "w") as f:
+#     f.write(origFile)
+
+# # keep tmpdir when error occured
+# shutil.rmtree(tmpdir)
