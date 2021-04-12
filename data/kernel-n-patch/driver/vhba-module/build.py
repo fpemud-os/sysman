47ef72d681514c2c0c28b221df08976832ca31d5
fpemud <fpemud@163.com>
2021-04-12 12:48:07 +0800

    Update build.py

--- data/kernel-n-patch/driver/vhba-module/build.py
+++ data/kernel-n-patch/driver/vhba-module/build.py
@@ -5,10 +5,8 @@ import sys
 import subprocess
 
 srcDir = os.path.join(sys.argv[1], "vhba-module")
-buildTmpDir = sys.argv[2]
-kernelVer = sys.argv[3]
+kernelVer = sys.argv[2]
 
-os.chdir(buildTmpDir)
-subprocess.run("/bin/cp -r %s/* %s" % (srcDir, buildTmpDir), shell=True)
+subprocess.run("/bin/cp -r %s/* ." % (srcDir), shell=True)
 subprocess.run("/usr/bin/make KERNELRELEASE=%s" % (kernelVer), shell=True)
 subprocess.run("/usr/bin/make install KERNELRELEASE=%s" % (kernelVer), shell=True)
