bf7c0830ba1df6993b25c83d5cbe45de691d382c
fpemud <fpemud@sina.com>
2021-05-17 20:38:22 +0800

    fix

--- data/kernel-n-patch/driver/vbox/build.py
+++ data/kernel-n-patch/driver/vbox/build.py
@@ -10,31 +10,7 @@ kernelVer = sys.argv[2]
 fnList = os.listdir(srcDir)
 if len(fnList) != 1:
     raise Exception("invalid source directory")
-fullfn = os.path.join(srcDir, fnList[0])
+subprocess.run("/bin/tar -xJf %s" % (os.path.join(srcDir, fnList[0])), shell=True)
 
-subprocess.run("/bin/tar -xJf %s" % (fullfn), shell=True)
-subprocess.run("/usr/bin/make KERNELRELEASE=%s" % (kernelVer), shell=True)
-subprocess.run("/usr/bin/make install KERNELRELEASE=%s" % (kernelVer), shell=True)
-
-
-
-
-
-
-#!/usr/bin/python3
-
-# import os
-# import sys
-# import subprocess
-
-# srcDir = sys.argv[1]
-# buildTmpDir = sys.argv[2]
-# kerelVer = sys.argv[3]
-
-# os.chdir(buildTmpDir)
-# subprocess.run("/bin/cp -r %s/* %s" % (srcDir, buildTmpDir), shell=True)
-# subprocess.run("/usr/bin/make KERN_DIR=\"%s\"" % (os.path.join("/lib/modules", kerelVer, "build")), shell=True)
-
-
-
-
+subprocess.run("/usr/bin/make KERN_VER=%s" % (kernelVer), shell=True)
+subprocess.run("/usr/bin/make install KERN_VER=%s" % (kernelVer), shell=True)
--- data/kernel-n-patch/driver/vhba-module/build.py
+++ data/kernel-n-patch/driver/vhba-module/build.py
@@ -8,5 +8,6 @@ srcDir = os.path.join(sys.argv[1], "vhba-module")
 kernelVer = sys.argv[2]
 
 subprocess.run("/bin/cp -r %s/* ." % (srcDir), shell=True)
+
 subprocess.run("/usr/bin/make KERNELRELEASE=%s" % (kernelVer), shell=True)
 subprocess.run("/usr/bin/make install KERNELRELEASE=%s" % (kernelVer), shell=True)
