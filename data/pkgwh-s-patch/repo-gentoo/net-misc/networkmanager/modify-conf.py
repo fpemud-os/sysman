b9f86bf24f77aaf5c23466843114e86d5aa5b712
fpemud <fpemud@sina.com>
2021-07-01 12:50:17 +0800

    fix

--- data/pkgwh-s-patch/repo-gentoo/net-misc/networkmanager/modify-conf.py
+++ data/pkgwh-s-patch/repo-gentoo/net-misc/networkmanager/modify-conf.py
@@ -1,63 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
-import os
 import re
 import glob
 import pathlib
-
-
-def compareVersion(verstr1, verstr2):
-    """eg: 3.9.11-r1 or 3.10.7"""
-
-    partList1 = verstr1.split("-")
-    partList2 = verstr2.split("-")
-
-    verList1 = partList1[0].split(".")
-    verList2 = partList2[0].split(".")
-
-    if len(verList1) == 3 and len(verList2) == 3:
-        ver1 = int(verList1[0]) * 10000 + int(verList1[1]) * 100 + int(verList1[2])
-        ver2 = int(verList2[0]) * 10000 + int(verList2[1]) * 100 + int(verList2[2])
-    elif len(verList1) == 2 and len(verList2) == 2:
-        ver1 = int(verList1[0]) * 100 + int(verList1[1])
-        ver2 = int(verList2[0]) * 100 + int(verList2[1])
-    elif len(verList1) == 1 and len(verList2) == 1:
-        ver1 = int(verList1[0])
-        ver2 = int(verList2[0])
-    else:
-        assert False
-
-    if ver1 > ver2:
-        return 1
-    elif ver1 < ver2:
-        return -1
-
-    if len(partList1) >= 2 and len(partList2) == 1:
-        return 1
-    elif len(partList1) == 1 and len(partList2) >= 2:
-        return -1
-
-    p1 = "-".join(partList1[1:])
-    p2 = "-".join(partList2[1:])
-    if p1 > p2:
-        return 1
-    elif p1 < p2:
-        return -1
-
-    return 0
-
-
-def removeFileInManifest(s):
-    filename = "Manifest"
-    buf = pathlib.Path(filename).read_text()
-    lineList = []
-    for line in buf.split("\n"):
-        if " " + s + " " not in line:
-            lineList.append(line)
-    with open(filename, "w") as f:
-        f.write("\n".join(lineList))
-
 
 try:
     # what to insert (with blank line in the beginning and the end)
@@ -70,17 +16,10 @@ echo "no-auto-default=*" >> "${ED}"/etc/NetworkManager/NetworkManager.conf
     buf2 = buf2.replace("\n", "\n\t")
     buf2 += "\n"
 
-    bFound = False
     for fn in glob.glob("*.ebuild"):
         m = re.fullmatch(".*?-([0-9-r\\.]+)\\.ebuild", fn)
         if m is None:
             raise ValueError()
-
-        if compareVersion(m.group(1), "1.26.0") < 0:
-            os.remove(fn)
-            removeFileInManifest(fn)
-            bFound = True
-            continue
 
         buf = pathlib.Path(fn).read_text()
 
@@ -97,7 +36,5 @@ echo "no-auto-default=*" >> "${ED}"/etc/NetworkManager/NetworkManager.conf
         buf = buf[:pos] + buf2 + buf[pos:]
         with open(fn, "w") as f:
             f.write(buf)
-    if not bFound:
-        raise ValueError()
 except ValueError:
     print("outdated")
