c8d4089283f0fb0acb39795e6ce1637bd255132f
fpemud <fpemud@sina.com>
2021-07-02 15:20:17 +0800

    fix

--- data/pkgwh-s-patch/repo-gentoo/dev-vcs/git/extend-patches.py
+++ data/pkgwh-s-patch/repo-gentoo/dev-vcs/git/extend-patches.py
@@ -8,7 +8,7 @@ import shutil
 selfDir = os.path.dirname(os.path.realpath(__file__))
 
 os.makedirs("files", exist_ok=True)
-shutil.copyfile(os.path.join(selfDir, "files", "extend-content.patch"), os.path.join("files", "extend-content.patch"))
+shutil.copyfile(os.path.join(selfDir, "files", "http-connection-timeout.patch"), os.path.join("files", "http-connection-timeout.patch"))
 for fn in glob.glob("*.ebuild"):
     with open(fn, "a") as f:
-        f.write('\nPATCHES=( ${PATCHES[@]} "${FILESDIR}"/extend-content.patch )\n')
+        f.write('\nPATCHES=( ${PATCHES[@]} "${FILESDIR}"/http-connection-timeout.patch )\n')
