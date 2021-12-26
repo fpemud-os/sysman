7fa28b46b0e2c9c68adce428c6c06547b690963e
fpemud <fpemud@sina.com>
2021-12-26 09:16:53 +0800

    fix

--- lib/helper_rescueos.py
+++ lib/helper_rescueos.py
@@ -8,6 +8,7 @@ import pathlib
 import gstage4
 import gstage4.seed_stages
 import gstage4.repositories
+import gstage4.target_features
 from fm_util import FmUtil
 from fm_util import TmpMount
 from fm_util import TempChdir
@@ -44,25 +45,24 @@ class RescueDiskBuilder:
 
     def build(self, hwInfo):
         s = gstage4.Settings()
-        s.set_program_name("fpemud-os-sysman")
-        s.set_host_computing_power(gstage4.ComputingPower.new(hwInfo.hwDict["cpu"]["cores"],
-                                                              hwInfo.hwDict["memory"]["size"] * 1024 * 1024 * 1024,
-                                                              10 if "fan" in hwInfo.hwDict else 1))
-        s["host_distfiles_dir"] = FmConst.distDir
+        s.program_name = "fpemud-os-sysman"
+        s.host_computing_power = gstage4.ComputingPower.new(hwInfo.hwDict["cpu"]["cores"],
+                                                            hwInfo.hwDict["memory"]["size"] * 1024 * 1024 * 1024,
+                                                            10 if "fan" in hwInfo.hwDict else 1)
+        s.host_distfiles_dir = FmConst.distDir
 
         ts = gstage4.TargetSettings()
-        ts["pkg_license"] = {
+        ts.pkg_license = {
             "*/*": "*",
         }
-        ts["world_set"] = [
-            "app-admin/eselect",
-            "app-eselect/eselect-timezone",
-            "app-editors/nano",
-            "sys-kernel/gentoo-sources",
-            "sys-kernel/genkernel",
-        ]
+
+        ft1 = gstage4.target_features.SshServer()
+        ft2 = gstage4.target_features.ChronyDaemon()
+        ft3 = gstage4.target_features.NetworkManager()
+        ft4 = gstage4.target_features.GettyAutoLogin()
 
         self.tmpDir.initialize()
+
         b = gstage4.Builder(s, ts, self.tmpDir)
 
         c = gstage4.seed_stages.GentooStage3Archive("/stage3-amd64-systemd-20211212T170613Z.tar.xz")
@@ -78,24 +78,35 @@ class RescueDiskBuilder:
         print("step3")
         b.action_init_confdir()
 
-        print("step6")
-        b.action_update_world_set()
+        print("step4")
+        worldSet = [
+            "app-admin/eselect",
+            "app-eselect/eselect-timezone",
+            "app-editors/nano",
+            "sys-kernel/gentoo-sources",
+            "sys-kernel/genkernel",
+            "sys-apps/portage",
+            "sys-apps/systemd",
+        ]
+        ft1.update_world_set(worldSet)
+        ft2.update_world_set(worldSet)
+        ft3.update_world_set(worldSet)
+        b.action_update_world(world_set=worldSet)
 
-        print("step7")
+        print("step5")
         b.action_install_kernel()
 
+        print("step6")
+        serviceList = []
+        ft1.update_service_list(serviceList)
+        ft2.update_service_list(serviceList)
+        ft3.update_service_list(serviceList)
+        b.action_enable_services(serviceList)
+
         print("step8")
-        fileList = []
-        if True:
-            for x in glob.glob(os.path.join(FmConst.libexecDir, "rescue-*")):
-                fileList.append((x, 0o755, "/root"))
-            fileList.append((os.path.join(self.filesDir, "getty-autologin.conf"), 0o644, "/etc/systemd/system/getty@.service.d"))
-        cmdList = [
-            "systemctl enable sshd",
-            "systemctl enable NetworkManager",
-            "systemctl enable chronyd",
-        ]
-        b.action_config_system(fileList, cmdList)
+        scriptList = []
+        ft4.update_custom_script_list(scriptList)
+        b.action_customize_system(scriptList)
 
         print("step9")
         b.action_cleanup()
