3a87eb3a48fa2ba970a7fc62f3d6743713ed7aab
fpemud <fpemud@sina.com>
2022-01-13 11:22:00 +0800

    fix

--- lib/helper_rescueos.py
+++ lib/helper_rescueos.py
@@ -195,7 +195,6 @@ class RescueDiskBuilder:
                 # "sys-fs/f2fs-tools",
                 "sys-fs/lsscsi",
                 "sys-fs/mtools",
-                "sys-fs/nilfs-utils",
                 # "sys-fs/ntfs3g",          # requires FUSE2 which is deprecated
                 "sys-fs/xfsdump",
                 "sys-fs/xfsprogs",
