3bd1190662f63e85207838b8ae3c9abc2134a7e0
fpemud <fpemud@sina.com>
2022-02-12 17:53:44 +0800

    fix

--- data/pkgwh-n-patch/repo-gentoo/net-p2p/bitcoind/remove-var.py
+++ data/pkgwh-n-patch/repo-gentoo/net-p2p/bitcoind/remove-var.py
@@ -8,8 +8,12 @@ try:
     for fn in glob.glob("*.ebuild"):
         newBuf = ""
         for line in pathlib.Path(fn).read_text().split("\n"):
-            if "/var/lib/bitcoin" not in line:
-                newBuf += line + "\n"
+            if "/var/lib/bitcoin" in line:
+                continue
+            if "systemd_newunit" in line:
+                newBuf += "\tsed -i 's#/var/lib/bitcoind#/var/cache/bitcoind#g' contrib/init/bitcoind.service\n"
+                newBuf += "\tsed -i 's#StateDirectory#CacheDirectory#g' contrib/init/bitcoind.service\n"
+            newBuf += line + "\n"
         with open(fn, "w") as f:
             f.write(newBuf)
 except ValueError:
